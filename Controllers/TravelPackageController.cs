8af7f819c001b5c754f28da6bb9a01518b5d0f45
GCaringi <giannicaringi94@gmail.com>
2022-10-20 14:39:03 +0200

    Add simple Routing and remove unnecessary code lines

---
+++ Controllers/TravelPackageController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_travel_agency.Controllers;
+
+public class TravelPackageController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Views/TravelPackage/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp_travel_agency.Views.TravelPackage;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
